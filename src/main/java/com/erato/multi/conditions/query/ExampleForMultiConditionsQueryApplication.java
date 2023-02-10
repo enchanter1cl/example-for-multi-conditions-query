15cceded967c53df6d97d3e708311cd8ddb43820
zhangYuan <zhangyuan@authing.cn>
2023-02-11 01:34:29 +0800

    init: Spring Boot project initialization

---
+++ src/main/java/com/erato/multi/conditions/query/ExampleForMultiConditionsQueryApplication.java
@@ -0,0 +1,13 @@
+package com.erato.multi.conditions.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleForMultiConditionsQueryApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleForMultiConditionsQueryApplication.class, args);
+    }
+    
+}
